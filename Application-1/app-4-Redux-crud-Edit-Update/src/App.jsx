4124e9d0ee55d375341e7e65b76da0a904613cc5
leelavathikm1 <leelavathikm1@gmail.com>
2026-01-29 14:23:04 +0530

    first

--- Application-1/app-4-Redux-crud-Edit-Update/src/App.jsx
+++ Application-1/app-4-Redux-crud-Edit-Update/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+// import { getAllUsersAsyncAction } from "./Store/UserSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const allUsers = useSelector((state) => {
+    console.log(state);
+    return state.usersDetails;
+  });
+  // const dispatch = useDispatch;
+  // const { users } = useSelector((store) => store.usersDetails);
+  // const [user, setUser] = useState({
+  //   id: "",
+  //   fname: "",
+  //   lname: "",
+  //   email: "",
+  //   state: "",
+  //   zip: "",
+  // });
+  // console.log(users);
+  // useEffect(() => {
+  //   dispatch(getAllUsersAsyncAction());
+  // }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Redux Crud Users Component</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Application-1/app-4-Redux-crud-Edit-Update/src/Store/UserSlice.js
+++ Application-1/app-4-Redux-crud-Edit-Update/src/Store/UserSlice.js
@@ -1,5 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+const initialState = {
+  users: [],
+};
 
+const API_URL = "http://localhost:3000/users";
 // export const createUserAyncAction = createAsyncThunk(
 //   "users/createUserAsync",
 //   async (user) => {
@@ -16,27 +20,20 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const getAllUsersAsyncAction = createAsyncThunk(
   "users/getAllUsersAsync",
   async () => {
-    const responce = await fetch("http://localhost:3000/users");
-
+    const responce = await fetch(API_URL);
     console.log("getAllUserasync Is Called");
-
     return responce.json();
   },
 );
 
 const UserSlice = createSlice({
-name:"users",
-initialState:initialState,
-extraReducers:(builder)=>{
-    builder.addCase(getAllUsersAsyncAction.fulfilled,(state,action)={
-        state.users=action.payload
-    })
-}
+  name: "users",
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getAllUsersAsyncAction.fulfilled, (state, action) => {
+      state.users = action.payload;
+    });
+  },
 });
-// name: "users",
-//   initialState: initialState,
-//   extraReducers: (builder) => {
-//     builder.addCase(getAllUserAsyncAction.fulfilled, (state, action) => {
-//       state.users = action.payload;
-//     });
+
 export default UserSlice.reducer;
